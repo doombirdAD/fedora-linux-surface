0bebc01459fac6551ce1fb5dcde5584698f66989
Tony Kelly <apatheticelation@gmail.com>
2019-05-31 19:58:24 -0500

    Updated and cleaned up setup.sh
    Added options to reduce the amount of disk space required and
    cleaned up some stuff.

--- setup.sh
+++ setup.sh
@@ -19,53 +19,46 @@ fi
 echo "Building kernel for Fedora ${RELEASE}.\n"
 sleep 3
 
-# CLONE REPOSITORIES
+## CLONE REPOSITORIES
+
+# Start with rpmdev-buildtree.  It's idempotent so it's not a problem to
+# run it multiple times.
+rpmdev-setuptree
 
 cd build
 
-## clone jakeday/linux-surface
+# clone jakeday/linux-surface
 git clone https://github.com/jakeday/linux-surface
 
-## clone Fedora kernel repository
-## (We use fedpkg to manage practically every aspect of package building 
-## on Fedora.)
-
-
-## TODO: The Fedora repo plus the kernel repo is 25+ GiB.  
-## Make sure we have space
-fedpkg clone -a kernel
+# clone Fedora kernel repository
+# (We use fedpkg to manage practically every aspect of package building
+# on Fedora.)
+fedpkg clone -a --branch ${RELEASE} kernel
 
 ## BUILD KERNEL PACKAGES
 
-cd kernel
-
-# create local branch $RELEASE and set it to track origin/${RELEASE}
-git checkout -b ${RELEASE}
-
-## Get kernel Maj.min version 
-## (needed to figure out correct patches to pull from jakeday repo)
-KERNELVER=$(fedpkg verrel | \
+# Get kernel Maj.min version
+# (needed to figure out correct patches to pull from jakeday repo)
+KERNELVER=$(fedpkg --path kernel verrel | \
   sed -e 's/^kernel-\([^.]*\)\.\([^.]*\)\..*$/\1.\2/')
 
-## Move jakeday patches to local directory
-cp ../linux-surface/patches/${KERNELVER}/*.patch .
+# Move jakeday patches to local directory
+cp linux-surface/patches/${KERNELVER}/*.patch kernel/
 
-##  Apply our patches to Fedora repository
-git am ../patches/${RELEASE}/*.patch
+cd kernel
+#  Apply our patches to Fedora repository
+git am ../../patches/${RELEASE}/*.patch
 
-## Build new configs based off of the changes to the config file tree
+# Build new configs based off of the changes to the config file tree
 configs/build_configs.sh
 
-## run the fedpkg command which will build the kernel packages
+# run the fedpkg command which will build the kernel packages
+# TODO: The kernel source is going to be 21+G, Make sure we have space
 fedpkg --release ${RELEASE} local
 
 # Copy RPMs to rpm/ directory
 cd ../
-mv kernel/x86_64/*.rpm rpms/
+mv kernel/x86_64/*.rpm ../rpms/
 
-## TODO: Collect firmware packages from jakeday/linux-surface 
-## into rpm packages.
-#for file in specsfiles/*.spec; do
-#  rpmbuild --define '_topdir build/rpmbuild' -ba $file
-#done
-	
+# TODO: Collect firmware packages from jakeday/linux-surface
+# into rpm packages.
